28784e7d8e8dbe35feae81d9bc84e31598709014
Shen <23107391+ShenPaul@users.noreply.github.com>
2021-01-19 03:14:19 -0500

    Update COVID database populator

--- populateCovidDb.py
+++ populateCovidDb.py
@@ -35,21 +35,41 @@ def main():
     df['Test_Reported_Date'] = pd.to_datetime(df['Test_Reported_Date'])
     df['Specimen_Date'] = pd.to_datetime(df['Specimen_Date'])
 
-    df = df.drop([0, 58])
+    df.fillna('No Data', inplace=True)
+
+    #df.drop(df.index[0:58], inplace=True)
 
     for row in df.itertuples(index=False, name=None):
+        if row[2] != 'No Data':
+            Accurate_Episode_DateField = row[2].strftime('%Y-%m-%d')
+        else:
+            Accurate_Episode_DateField = row[2]
+        if row[3] != 'No Data':
+            Case_Reported_DateField = row[3].strftime('%Y-%m-%d')
+        else:
+            Case_Reported_DateField = row[3]
+        if row[4] != 'No Data':
+            Test_Reported_DateField = row[4].strftime('%Y-%m-%d')
+        else:
+            Test_Reported_DateField = row[4]
+        if row[5] != 'No Data':
+            Specimen_DateField = row[5].strftime('%Y-%m-%d')
+        else:
+            Specimen_DateField = row[5]
+
+
         jsonDocument = {
             # "_idField": row[0],
             "Row_IDField": row[1],
-            "Accurate_Episode_DateField": row[2].strftime('%Y-%m-%d'),
-            "Case_Reported_DateField": row[3].strftime('%Y-%m-%d'),
-            "Test_Reported_DateField": row[4].strftime('%Y-%m-%d'),
-            "Specimen_DateField": row[5].strftime('%Y-%m-%d'),
+            "Accurate_Episode_DateField": Accurate_Episode_DateField,
+            "Case_Reported_DateField": Case_Reported_DateField,
+            "Test_Reported_DateField": Test_Reported_DateField,
+            "Specimen_DateField": Specimen_DateField,
             "Age_GroupField": row[6].replace('s', ''),
             "Client_GenderField": row[7],
             "Case_AcquisitionInfoField": row[8],
             "Outcome1Field": row[9],
-            # "Outbreak_RelatedField": row[10], gives an error bc its empty
+            "Outbreak_RelatedField": row[10],
             "Reporting_PHU_IDField": row[11],
             "Reporting_PHUField": row[12],
             "Reporting_PHU_AddressField": row[13],
